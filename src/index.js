ae828ef7cb81672d1860e87dc09a68fea65fd3ea
ZodiacGemini <alexanderedvinsson@icloud.com>
2017-06-02 17:18:33 +0200

    highscore

--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ var height = 0;
 var width = 0;
 var array = [];
 var flagCounter = 0;
+var timer = 1;
+var startTimer;
+var topTenScore = [];
+var database = firebase.database();
+
+
+function AddHighScore(number, name) {
+    var hsKey = database.ref().child('highscore').push().key;
+    var highScore = {score: number, username: name};
+    var updates = {};
+    updates['/highscore/' + width + height + '/' + hsKey] = highScore;
+    return database.ref().update(updates)
+}
 
 function UpdateCounterText(){
-    var nextCounter = array.reduce((p, c) => {
-        let counter = 0;
-        c.forEach(o => o.flag ? counter++ : o);
-        return p - counter;
-    }, mines);
+    var nextCounter = array
+    .reduce((p, c) => p - c.filter(o => o.flag).length, mines);
     $('#counterText').text(nextCounter)
     flagCounter = nextCounter;
+    
 };
-
 
 function StartGame() {
     for (var i = 0; i < height; i++) {
@@ -58,7 +68,6 @@ function StartGame() {
         }
     };
 
-    GetSurroundingMines(array, 0, 0);
     function GetSurroundingMines(array, i, j) {
         var simonCells = [[i + 1, j], [i + 1, j + 1], [i + 1, j - 1], [i - 1, j], [i - 1, j - 1], [i - 1, j + 1], [i, j + 1], [i, j - 1]];
 
@@ -75,13 +84,10 @@ function StartGame() {
         var numberOfMines = realCells.filter(function (array) {
             return array.mine;
         })
-
         return numberOfMines;
     };
     //Reset counter and button picture
-    UpdateCounterText()
-    $('#playAgain').attr('class', 'playAgain')
-
+    Reset();
     render();
     function render() {
         $mineSweeper.empty();
@@ -100,19 +106,19 @@ function StartGame() {
         }
     };
 
-    function OpenSurroundingCells(array, i, j) {
-        var simonCells = [[i + 1, j], [i + 1, j + 1], [i + 1, j - 1], [i - 1, j], [i - 1, j - 1], [i - 1, j + 1], [i, j + 1], [i, j - 1]];
-        var validCells = simonCells.filter(function (cell) {
+    function OpenSurroundingCells(i, j) {
+        var surroundingCells = [[i + 1, j], [i + 1, j + 1], [i + 1, j - 1], [i - 1, j], [i - 1, j - 1], [i - 1, j + 1], [i, j + 1], [i, j - 1]];
+        var validCells = surroundingCells.filter((cell) => {
             var i = cell[0]
             var j = cell[1]
             return i >= 0 && i < array.length && j >= 0 && j < array[0].length
         });
-        var cellsForRecursion = validCells.filter(function (cell) {
+        var cellsForRecursion = validCells.filter((cell) => {
             i = cell[0]
             j = cell[1]
             return array[i][j].open == false && array[i][j].number == 0;
         });
-        validCells.forEach(function (cell) {
+        validCells.forEach((cell) => {
             i = cell[0]
             j = cell[1]
             array[i][j].open = true;
@@ -122,11 +128,54 @@ function StartGame() {
         return cellsForRecursion;
     };
 
+    function OpenSurroundingNonFlaggedCells(i, j) {
+        var surroundingCells = [[i + 1, j], [i + 1, j + 1], [i + 1, j - 1], [i - 1, j], [i - 1, j - 1], [i - 1, j + 1], [i, j + 1], [i, j - 1]];
+        var validCells = surroundingCells.filter((cell) => {
+            let k = cell[0]
+            let l = cell[1]
+            return k >= 0 && k < array.length && l >= 0 && l < array[0].length
+        });
+        var flagCells = validCells.filter((cell) => {
+            let k = cell[0]
+            let l = cell[1]
+            return array[k][l].flag
+        });
+        if(flagCells.length == array[i][j].number){
+            validCells.forEach((cell) => {
+                let k = cell[0];
+                let l = cell[1];
+                if(!array[k][l].flag){
+                    if (array[k][l].mine) {
+                        array.forEach(r => r.forEach(cell => cell.open = true));
+                        $('.playAgain').attr('class', 'userLost')
+                        clearInterval(startTimer);
+                    };
+                    if (array[k][l].number == 0 && !array[k][l].mine) {
+                        ClickedOnZero(k, l);
+                    };
+                    array[k][l].open = true;
+                    array[k][l].flag = false;
+                    array[k][l].ask = false;
+                }
+            })
+        }
+        if(UserWon(i, j))
+            clearInterval(startTimer);
+        render();
+    };
+
+    $(document).on('dblclick', '.opencell', function (e) {
+        e.stopImmediatePropagation();
+        var $cell = $(this);
+        var i = $cell.data('i');
+        var j = $cell.data('j');
+        OpenSurroundingNonFlaggedCells(i, j);
+    });
+
     $(document).on('contextmenu', '.flagCell, .cell, .askCell', function (event) {
         event.preventDefault();
         event.stopImmediatePropagation();
-        var $cell = $(this);  
-
+        var $cell = $(this);
         const setCellClass = (n) => $cell.attr('class', n);
 
         var i = $cell.data('i');
@@ -151,6 +200,8 @@ function StartGame() {
                 array[i][j].ask = false;                             
                 setCellClass('cell')
                 break;
+            default:
+                break;
         };
         UpdateCounterText();          
     })
@@ -161,47 +212,57 @@ function StartGame() {
         var i = $cell.data('i');
         var j = $cell.data('j');
 
-
-        var closedCells = array.reduce((p, c) => {
-            let numberOfClosedCells = 0;
-            c.forEach(o => o.open ? o : numberOfClosedCells++);
-            return p + numberOfClosedCells;
-        }, 0);
-      
-        if (closedCells == mines + 1 && array[i][j].mine == false) {
-            array[i][j].open = true;
-            render();
-            $('.playAgain').toggleClass('userWon')
-            array.forEach(r => r.forEach(cell => cell.open = true));
-        }
-
-        else if (array[i][j].number != 0) {
+        if (array[i][j].number != 0) {
             array[i][j].open = true;
         }
-
         else if (array[i][j].number == 0 && !array[i][j].mine) {
-            array[i][j].open = true;
-            var allCellsToCheck = OpenSurroundingCells(array, i, j);
-            var loopingLength = allCellsToCheck;
-            for (var k = 0; k < loopingLength.length; k++) {
-                i = allCellsToCheck[k][0];
-                j = allCellsToCheck[k][1];
-                var returnedCells = OpenSurroundingCells(array, i, j);
-                if (returnedCells.length > 0) {
-                    for (var l = 0; l < returnedCells.length; l++) {
-                        loopingLength.push(returnedCells[l]);
-                    }
-                }
-            }
+            ClickedOnZero(i, j);
         }
 
         else if (array[i][j].mine) {
             array.forEach(r => r.forEach(cell => cell.open = true));
-            $('.playAgain').toggleClass('userLost')
+            $('.playAgain').attr('class', 'userLost')
+            clearInterval(startTimer);
         };
+
+        if(UserWon(i, j)){
+            clearInterval(startTimer);
+        }
+       
         UpdateCounterText();
         render();
     })
+
+    function ClickedOnZero(i, j) {
+        array[i][j].open = true;
+            var allCellsToCheck = OpenSurroundingCells(i, j);
+            var loopingLength = allCellsToCheck;
+            for (var k = 0; k < loopingLength.length; k++) {
+                i = allCellsToCheck[k][0];
+                j = allCellsToCheck[k][1];
+                var returnedCells = OpenSurroundingCells(i, j);
+                if (returnedCells.length > 0) {
+                    for (var l = 0; l < returnedCells.length; l++) {
+                        loopingLength.push(returnedCells[l]);
+                }
+            }
+        }
+    }
+
+    function UserWon(i, j) {
+        var closedCells = array
+        .reduce((p, c) => c.filter(o => !o.open).length + p, 0);
+        if (closedCells == mines && array[i][j].mine == false) {
+            array[i][j].open = true;
+            $('.playAgain').attr('class', 'userWon')
+            array.forEach(r => r.forEach(cell => cell.open = true));
+            render();
+            AskForHighScoreSubmit();
+            return true;
+        }
+        else
+            return false;
+    }
 }
 
 function SetUpGame(){
@@ -209,6 +270,7 @@ function SetUpGame(){
     height = parseInt(height);
     width = $('#width').val();
     width = parseInt(width);
+    
     if(height >= 2 && width >= 2 && height <= 50 && width <= 50){
         mines = Math.round((width * height) / 8) + 1;
         $('#size').toggle();
@@ -219,4 +281,63 @@ function SetUpGame(){
         $('#chooseSize').html('<p style="color: red;">Must be between 2 and 50<p/>');
         return;
     }
+}
+
+function AskForHighScoreSubmit() {
+    if(topTenScore.length >= 10){
+        if(topTenScore[9].score > timer){
+                if(confirm('Top ten! Submit highscore?')){
+                    var name = prompt('Choose name (max 10 characters)')
+                    while(true){
+                        if(name.length <= 10){
+                            AddHighScore(timer, name);
+                            break;
+                    }
+                        else{
+                            name = prompt('Max 10 characters');
+                    }
+                }
+            }
+        }
+        else{
+            alert('Too bad for highscore');
+        }
+    }
+    else{
+        if(confirm('Top ten! Submit highscore?')){
+            var name = prompt('Choose name (max 10 characters)')
+            while(true){
+                if(name.length <= 10){
+                    AddHighScore(timer, name);
+                    break;
+                }
+                else{
+                    name = prompt('Max 10 characters');
+                }
+            }
+        }
+    }
+}
+
+function Reset() {
+    UpdateCounterText()
+    $('#playAgain').attr('class', 'playAgain')
+    try {clearInterval(startTimer);} catch (error) {}
+    timer = 1;
+    $('#timer').text(timer);
+    $('#hsBody').empty();
+    $('#highScoreText').text('Lowscore ' + width + ' x ' + height);
+
+    startTimer = setInterval(() => {timer++; $('#timer').text(timer)}, 1000);
+
+    database.ref('/highscore/' + width + height + '/').once('value').then(function(snapshot) {
+        let obj = snapshot.val()
+        for(var key in obj){
+            topTenScore.push({username: obj[key].username, score: obj[key].score});
+        }
+        topTenScore = topTenScore.sort((a, b) => a.score - b.score).slice(0, 10);
+        topTenScore.forEach(c => {
+            $('#hsBody').append($('<tr/>', {html: '<td>' + c.username + '</td><td class="scoreTd">' + c.score + '</td>'}))
+        })
+    });
 }
