d54ea20aafab11d345db0f1b4862b3ed6e28d307
ZodiacGemini <alexanderedvinsson@icloud.com>
2017-06-04 20:49:29 +0200

    ha

--- index.js
+++ index.js
@@ -127,7 +127,6 @@ function StartGame() {
     };
 
     $(document).on('dblclick', '.opencell', (e) => TriggerDoubleClick(e));
-    $(document).on('doubletap', '#flagButton, .flagCell, .cell, .askCell', function(e){ return false;});
     $(document).on('doubletap', '.opencell', (e) => TriggerDoubleClick(e));
     $(document).on('contextmenu', '.flagCell, .cell, .askCell', function(e) { TriggerRightClick(e)});
     $(document).on('contextmenu', '.opencell', function(e){return false;});
